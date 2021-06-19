2fcc5641a95f9a398a8f2d41363b7cc7ea8dbcab
Bhavyai Gupta <zbhavyai@gmail.com>
2021-06-20 03:45:06 +0530

    introducing skipping
    skip some/all repos from update by listing them in skip_file.txt

--- README.md
+++ README.md
@@ -3,28 +3,45 @@
 Purpose of this script is to run a `git pull --ff-only` on the active branch for all the repositories in the directory hardcoded in the script or supplied as a parameter to the script
 
 
-## Requirements
+
+## Features
+
++ Run `git pull --ff-only` quickly on all the repositories contained in a directory
+
++ If some repositories need to be excluded, simply enlist them in the file `skip_file.txt`, and place this file in the project directory
+
+
+
+## Dependencies
 
 + git or git bash installed
+
 + configured upstream in the repository
 
 
-## Running Locally
 
-+ Clone this repository
+## How to use
+
++ Clone this repository on your local machine or extract the downloaded the zip archive
+
 + Let's say you have some git repos in the folder `/home/user/repos/` which you want to sync with the remote repo
+
 + Run the command in shell or git bash
    ```shell
    ./update_repos.sh /home/user/repos
    ```
+
 + If you would like to run the script without any parameter, edit the script to change the variable `DEFAULT_DIRECTORY` at the beginning of the script, and make it point to the local repo in your disk
+
 + Once the change is done, simply run the command
    ```shell
    ./update_repos.sh
    ```
 
 
+
 ## To note
 
 + In case of errors during the process, this script simply continue until it ends
+
 + Doesn't handle the merge conflicts or sync errors, but displays the message, so that necessary step can be taken
--- update_repos.sh
+++ update_repos.sh
@@ -16,89 +16,186 @@
 # ./update_repos.sh
 
 
-DEFAULT_DIRECTORY="/d/GitHub/university-calgary"
+
+# declare the variables
+DEFAULT_DIRECTORY="/d/GitHub/"
+ORG_DIRECTORY="$( pwd )"
+SKIP_FILE="skip_file.txt"
 COLOR="\e[0;91m"
 RESET="\e[0m"
+declare -a skip_list
+index=0
 
 
-# check if the directory was passed as a parameter to the script
-# else default to the hardcoded value of directory
-if [[ -z $1 ]]							# check if string passed to script is of 0 length (basically not passed)
+
+# check if string passed to script is of 0 length (basically not passed)
+if [[ -z ${1} ]]
 then
-	ROOT="${DEFAULT_DIRECTORY}"
-elif [[ -d $1 ]]						# check if the string passed is a directory
+    # default to the hardcoded directory
+    ROOT="${DEFAULT_DIRECTORY}"
+
+# check if the string passed is a directory
+elif [[ -d ${1} ]]
 then
-	ROOT=$1
+    # set the ROOT to the argument
+    ROOT=${1}
+
+# exit if argument was passed but not a valid directory
 else
-	echo "Fatal! Parameter passed to the script is not a directory"
-	echo "exit"
-	exit 1
+    echo "Fatal! Parameter passed to the script is not a directory"
+    echo "exit"
+    exit 1
 fi
+
 
 
 # cd to the directory holding git repos
-# if fail, exit the script
-cd "${ROOT}" 2> /dev/null								# remove the errors of cd, as it is handled by if
-if [[ $? -ne 0 ]]										# if cd was not successful, exit
+# remove the errors of cd, as it is handled by if
+cd "${ROOT}" 2> /dev/null
+
+# if cd was not successful, exit
+if [[ ${?} -ne 0 ]]
 then
-	echo "Fatal! Error in accessing ${ROOT}"
-	echo "exit"
-	exit 1
+    echo "Fatal! Error in accessing ${ROOT}"
+    echo "exit"
+    exit 1
 fi
 
 
-ROOT="$( pwd )"											# reassigning to handle relative paths
+
+# reassigning to handle relative paths
+ROOT="$( pwd )"
+
+
+
+# function to read lines of the SKIP_FILE into array skip_list
+# --------------------------------------------------------------------------------
+load_skip_file()
+{
+    # start reading the file only if it exists
+    if [[ -f "${ORG_DIRECTORY}/${SKIP_FILE}" ]]
+    then
+        # IFS= (or IFS='') prevents leading/trailing whitespace from being trimmed
+        # -r prevents backslash escapes from being interpreted
+        # || [[ -n $line ]] prevents the last line from being ignored if it doesn't end with a \n
+        # More details here - https://stackoverflow.com/a/10929511/16018083
+        while IFS= read -r line || [[ -n "${line}" ]];
+        do
+            # process the line only if it represents a directory
+            if [[ -d ${line} ]]
+            then
+                # store the absolute path of the directory location read in the array at ${index}
+                skip_list["${index}"]="$(realpath "${line}")"
+            fi
+
+            # increment the index
+            index=$(("${index}" + 1))
+        done < "${ORG_DIRECTORY}/${SKIP_FILE}"
+    fi
+}
+# --------------------------------------------------------------------------------
+
+
+# function to check if the argument is in the array skip_list
+# --------------------------------------------------------------------------------
+check_if_to_be_skipped()
+{
+    found="false"
+
+    # loop for all elements in the array
+    for i in "${skip_list[@]}";
+    do
+        # check if parameter passed is in the array
+        if [ "${1}" = "${i}" ];
+        then
+            found="true"
+        fi
+    done
+
+    echo ${found}
+}
+# --------------------------------------------------------------------------------
+
 
 
 # function to navigate into directories and subdirectories
+# --------------------------------------------------------------------------------
 navigator()
 {
-	# printf "\n[INFO] Working on ${1}\n"				# print this for debug only
+    # print this for debug only
+    # printf "\n[INFO] Working on ${1}\n"
 
-	is_repo=$(is_git_repo)								# check if current directory is a git repo
+    # if the repository is to be skipped from updating
+    if [[ "$(check_if_to_be_skipped ${1})" = "true" ]]
+    then
+        # printf "\n[INFO] Skipping ${1}\n"
+        return 0
+    fi
 
-	if [[ "${is_repo}" = "true" ]]						# if a git repo, update it
-	then
-		update_repo "$1"
-		return 0										# terminate the function as repo inside repo is currently not supported
-	else
-		for ITEM in *;									# if not a git repo, loop through all files and folders inside working directory
-		do
-			ITEM_PATH="$1/${ITEM}"
+    # check if current directory is a git repo
+    is_repo=$(is_git_repo)
 
-			if [[ -d "${ITEM_PATH}" ]]					# if a directory is found, go inside it and repeat the process
-			then
-				cd "${ITEM_PATH}"
-				navigator "${ITEM_PATH}"
-				cd ../									# come out once every recursive call is made
-			fi
-		done
-	fi
+    # if a git repo, update it
+    if [[ "${is_repo}" = "true" ]]
+    then
+        update_repo "${1}"
+        # terminate the function as repo inside repo is currently not supported
+        return 0
+    else
+        # if not a git repo, loop through all files and folders inside working directory
+        for ITEM in *;
+        do
+            ITEM_PATH="${1}/${ITEM}"
+
+            # if a directory is found, go inside it and repeat the process
+            if [[ -d "${ITEM_PATH}" ]]
+            then
+                cd "${ITEM_PATH}"
+                navigator "${ITEM_PATH}"
+
+                # come out once every recursive call is made
+                cd ../
+            fi
+        done
+    fi
 }
+# --------------------------------------------------------------------------------
+
 
 
 # function to check if working directory is a git repo
+# --------------------------------------------------------------------------------
 is_git_repo()
 {
-	is_repo="$( git rev-parse --is-inside-work-tree 2>/dev/null )"
+    is_repo="$( git rev-parse --is-inside-work-tree 2>/dev/null )"
 
-	if [[ "$is_repo" = "true" ]]
-	then
-		echo "true"
-	else
-		echo "false"
-	fi
+    if [[ "${is_repo}" = "true" ]]
+    then
+        echo "true"
+    else
+        echo "false"
+    fi
 }
+# --------------------------------------------------------------------------------
+
 
 
 # function to pull the remote repo
+# --------------------------------------------------------------------------------
 update_repo()
 {
-	printf "\n[${COLOR}UPDATING${RESET}] ${1}\n\n"
-	printf "$( git pull --ff-only )"					# pull is fast forward so that no unusual changes are done in local repo
+    printf "\n[${COLOR}UPDATING${RESET}] ${1}\n\n"
+     # pull is fast forward so that no unusual changes are done in local repo
+    printf "$( git pull --ff-only )"
 
-	printf "\n\n"
+    printf "\n\n"
 }
+# --------------------------------------------------------------------------------
 
 
-navigator "$ROOT"; exit									# call to the main function of the script
+
+# load the SKIP_FILE
+load_skip_file
+
+# update the repos
+navigator "${ROOT}"; exit
